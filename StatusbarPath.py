a5737f64640993c62d95ea71a5b2d892ff7991dc
Michael Hogg <michael.hogg.uk@gmail.com>
2015-01-21 19:27:57 +0000

    Add coloured emoji symbols around filename

--- StatusbarPath.py
+++ StatusbarPath.py
@@ -13,4 +13,10 @@ class CurrentPathStatusCommand(sublime_plugin.EventListener):
             if 'HOME' in os.environ:
                     filename = filename.replace(os.environ['HOME'], '~', 1)
 
-            view.set_status('zPath', filename)
+            emojiBlue   = u'\u27BF'
+            emojiGreen  = u'\u2733\uFE0F'
+            emojiOrange = u'\u2734\uFE0F'
+
+            status = emojiBlue + emojiGreen + emojiOrange + ' ' + filename + ' ' + emojiOrange + emojiGreen + emojiBlue
+
+            view.set_status('zPath', status)
